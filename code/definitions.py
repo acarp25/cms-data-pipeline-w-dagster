2cae0448a4359626974382a0dfa099e433b49e3b
Alex Carpenito <acarp25@gmail.com>
2025-11-20 23:05:01 -0500

    successfully loading cms file to table

--- code/definitions.py
+++ code/definitions.py
@@ -1,17 +1,17 @@
 import dagster as dg
 from dagster_duckdb import DuckDBResource
 
-from .ingestion.cms import assets as cms_assets
+from .ingestion.cms import assets as cms_assets, jobs as cms_jobs
 
 all_assets = dg.load_assets_from_modules([cms_assets])
 
 defs = dg.Definitions(
     assets=all_assets,
-    # jobs=[complex_job, hello_cereal_job],
+    jobs=[cms_jobs.cms_refresh],
     # schedules=[every_weekday_9am],
     resources={
         "duckdb": DuckDBResource(
-            database="./data/duckdb/cms_data.duckdb",
+            database="data/duckdb/cms_data.duckdb",
         ),
     }
 )
